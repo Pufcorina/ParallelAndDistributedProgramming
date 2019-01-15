5aebeb349aaa641eca81741d5fb33488d745c973
Corina Todoran <todorananacorina13@gmail.com>
2019-01-15 14:27:31 +0200

    lab 10

---
+++ laboratory10/DSM.cs
@@ -0,0 +1,183 @@
+﻿using MPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPD_MPI
+{
+    [Serializable]
+    class Msg
+    {
+        public UpdateMsg updateMsg = null;
+        public ChangeMsg changeMsg = null;
+        public SubscribeMsg subscribeMsg = null;
+
+        public bool exit = false; 
+
+        public Msg(UpdateMsg updateMsg)
+        {
+            this.updateMsg = updateMsg;
+        }
+
+        public Msg(ChangeMsg changeMsg)
+        {
+            this.changeMsg = changeMsg;
+        }
+       
+        public Msg(SubscribeMsg subscribeMsg)
+        {
+            this.subscribeMsg = subscribeMsg; 
+        }
+
+        public Msg(bool exit)
+        {
+            this.exit = exit; 
+        }
+    }
+
+    [Serializable]
+    class SubscribeMsg
+    {
+        public string var;
+        public int rank; 
+
+        public SubscribeMsg(string var, int rank)
+        {
+            this.var = var;
+            this.rank = rank; 
+        }
+    }
+
+    [Serializable]
+    class UpdateMsg
+    {
+        public string var;
+        public int val;
+
+        public UpdateMsg(string var, int val)
+        {
+            this.var = var;
+            this.val = val; 
+        }
+    }
+
+    [Serializable]
+    class ChangeMsg
+    {
+        public string var;
+        public int oldVal;
+        public int newVal; 
+        
+        public ChangeMsg(string var, int oldVal, int newVal)
+        {
+            this.var = var;
+            this.oldVal = oldVal;
+            this.newVal = newVal; 
+        }
+    }
+
+    class DSM
+    {
+        public int a = 1, b = 2, c = 3;
+        public Dictionary<String, List<int>> subscribers = new Dictionary<string, List<int>>(); 
+
+        public DSM() {
+            subscribers.Add("a", new List<int>());
+            subscribers.Add("b", new List<int>());
+            subscribers.Add("c", new List<int>());
+        }
+
+        public void updateVar(string var, int val)
+        {
+
+            this.setVar(var, val); 
+            UpdateMsg updateMsg = new UpdateMsg(var, val);
+            Msg msg = new Msg(updateMsg);
+
+            this.sendToSubscribers(var, msg); 
+        }
+
+        public void close()
+        {
+            this.sendAll(new Msg(true)); 
+        }
+
+        public void sendAll(Msg msg)
+        {
+            for (int i = 0; i < Communicator.world.Size; i++)
+            {
+                if (Communicator.world.Rank == i) continue;
+                Communicator.world.Send(msg, i, 0);
+            }
+        }
+
+        public void setVar(string var, int val)
+        {
+            if (var == "a") a = val; 
+            if (var == "b") b = val; 
+            if (var == "c") c = val; 
+        }
+
+        public void subscribeTo(string var)
+        {
+            this.subscribers[var].Add(Communicator.world.Rank);
+
+            this.sendAll(new Msg(new SubscribeMsg(var, Communicator.world.Rank))); 
+        }
+
+        public void subscribeOther(string var, int rank)
+        {
+            this.subscribers[var].Add(rank); 
+        }
+
+        public void sendToSubscribers(string var, Msg msg)
+        {
+            for (int i = 0; i < Communicator.world.Size; i++)
+            {
+                if (Communicator.world.Rank == i) continue;
+                if (!isSubscribedTo(var, i)) continue; 
+
+                Communicator.world.Send(msg, i, 0);
+            }
+        }
+
+        public bool isSubscribedTo(string var, int rank)
+        {
+            if (subscribers[var].Contains(rank))
+            {
+                return true;
+            }
+
+            return false; 
+        }
+
+        internal void checkAndReplace(string var, int val, int newVal)
+        {
+           if (var == "a")
+            {
+                if (a == val)
+                {
+                    updateVar("a", newVal);
+                }
+            }
+
+            if (var == "b")
+            {
+                if (b == val)
+                {
+                    updateVar("b", newVal);
+                }
+            }
+
+            if (var == "c")
+            {
+                if (a == val)
+                {
+                    updateVar("c", newVal);
+                }
+            }
+        }
+    }
+}
---
+++ laboratory10/MainProgram.cs
@@ -0,0 +1,140 @@
+﻿using MPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PPD_MPI
+{
+    class MainProgram
+    {
+
+        static void listenerThread(Object obj)
+        {
+            DSM dsm = (DSM) obj;
+
+            while (true)
+            {
+                Console.WriteLine("Rank " + Communicator.world.Rank.ToString() + " waiting ");
+                Msg msg = Communicator.world.Receive<Msg>(Communicator.anySource, Communicator.anyTag);
+
+                if (msg.exit) break;
+
+                if (msg.updateMsg != null)
+                {
+                    Console.WriteLine("Rank " + Communicator.world.Rank + " received : " + msg.updateMsg.var + " -> " + msg.updateMsg.val);
+                    dsm.setVar(msg.updateMsg.var, msg.updateMsg.val);
+                }
+
+                if (msg.subscribeMsg != null)
+                {
+                    Console.WriteLine("Rank " + Communicator.world.Rank + "received: " + msg.subscribeMsg.rank + " sub to " + msg.subscribeMsg.var);
+                    dsm.subscribeOther(msg.subscribeMsg.var, msg.subscribeMsg.rank);
+
+                }
+                writeVars(dsm);
+            }
+        }
+
+        static void writeVars(DSM dsm)
+        {
+            Console.Write("Rank " + Communicator.world.Rank + " a= " + dsm.a + " b= " + dsm.b + " c= " + dsm.c + " subs: ");
+            foreach (string var in dsm.subscribers.Keys)
+            {
+                Console.Write(var + ": [ ");
+                foreach (int rank in dsm.subscribers[var])
+                {
+                    Console.Write(rank + " "); 
+                }
+
+                Console.Write("] ");
+            }
+
+            Console.WriteLine(); 
+        }
+
+        static void Main(string[] args)
+        {
+            using (new MPI.Environment(ref args))
+            {
+                DSM dsm = new DSM();
+
+                if (Communicator.world.Rank == 0)
+                {
+
+                    Thread thread = new Thread(listenerThread);
+                    thread.Start(dsm); 
+
+                    bool exit = false;
+
+                    dsm.subscribeTo("a"); 
+                    dsm.subscribeTo("b"); 
+                    dsm.subscribeTo("c"); 
+
+                    while (!exit)
+                    {
+                        Console.WriteLine("1. Set var");
+                        Console.WriteLine("2. Change var");
+                        Console.WriteLine("0. Exit");
+
+                        int answer;
+                        int.TryParse(Console.ReadLine(), out answer);
+
+                        if (answer == 0)
+                        {
+                            dsm.close(); 
+                            exit = true;
+                        }else if (answer == 1)
+                        {
+                            Console.WriteLine("var (a, b, c) = ");
+                            string var = Console.ReadLine();
+
+                            Console.WriteLine("val (int) = ");
+                            int val;
+                            int.TryParse(Console.ReadLine(), out val);
+
+                            dsm.updateVar(var, val);
+                            writeVars(dsm);
+                        }else if (answer == 2)
+                        {
+                            Console.WriteLine("var to check (a, b, c) = ");
+                            string var = Console.ReadLine();
+
+                            Console.WriteLine("val to check (int) = ");
+                            int val;
+                            int.TryParse(Console.ReadLine(), out val);
+
+                            Console.WriteLine("val to check (int) = ");
+                            int newVal;
+                            int.TryParse(Console.ReadLine(), out newVal);
+
+                            dsm.checkAndReplace(var, val, newVal); 
+                        }
+                    }
+                   
+                }else if (Communicator.world.Rank == 1)
+                {
+
+                    Thread thread = new Thread(listenerThread);
+                    thread.Start(dsm);
+
+                    dsm.subscribeTo("a");
+
+                    thread.Join(); 
+
+                }
+                else if (Communicator.world.Rank == 2)
+                {
+                    Thread thread = new Thread(listenerThread);
+                    thread.Start(dsm);
+
+                    dsm.subscribeTo("b");
+
+                    thread.Join(); 
+                }
+            }
+        }
+    }
+}
